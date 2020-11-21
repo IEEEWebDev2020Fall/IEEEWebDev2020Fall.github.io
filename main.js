d2b87b24fabb8d70bff50a136401279f4d64bbe9
bixxyy <sodhibisman@gmail.com>
2020-11-21 14:27:42 -0800

    test bars work

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ selected.addEventListener("click", () => {
     optionsContainer.classList.toggle("active")
 })
 
-
+//Global
+const array = [];
 
 function randomNumArray(min, max){
 
@@ -15,10 +16,9 @@ function randomNumArray(min, max){
 }
 
 function setUp(x){
+    //generates array of random number
     x = document.getElementById("myRange").value;
-    
-    
-    const array = [];
+    //const array = [];
     for (let i = 0; i < x; i++){
         array.push(randomNumArray(5,10))
     }
@@ -27,16 +27,57 @@ function setUp(x){
 }
 
 function numOfBars(){
+    //setUp function generates array
+    //numOfBars function changes the array everytime slider is moved. 
     x = document.getElementById("myRange").value;
-    //document.getElementById("demo").innerHTML = x;
     var val = x.value;
-
     setUp(val);
+    displayBars();
 
 }
 
 
 document.getElementById("demo").innerHTML = setUp();
+
+
+function displayBars(){
+
+   // var svgWidth = svg.attr("width");
+   // var svgHeight = svg.attr("height");
+    var svgWidth = 500;
+    var svgHeight = 300;
+   var barPadding = 5;
+    var barWidth = (svgWidth / array.length);
+   /* var xScale = d3.scaleBand()
+        .domain(d3.range(array.length))
+        .rangeRound([50, svgWidth - 5]).padding(0.4);
+    var yScale = d3.scaleLinear()
+        .domain(d3.extend(array))
+        .range ([30, svgHeight - 30]);
+
+*/
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+    var svg = d3.select('svg')
+       .attr("width", svgWidth)
+       .attr("height", svgHeight);
+
+    var barChart = svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("y", function(d){
+            return svgHeight - 10 - d;
+        }) 
+        .attr("height", function(d){
+            return d;
+        })
+        .attr("width", barWidth - barPadding)
+        .attr("transform", function(d,i){
+            var translate = [barWidth * i, 0];
+            return "translate(" + translate +")";
+        });
+}
 
 
 
